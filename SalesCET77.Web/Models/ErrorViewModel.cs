428478eb8c1c55819d59a4b9c8b20b67ca731a3c
FelyppeRau <felypperau@gmail.com>
2023-05-24 16:18:05 +0100

    Ini

---
+++ SalesCET77.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesCET77.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
